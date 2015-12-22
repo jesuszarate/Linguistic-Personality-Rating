1b9d51a0c70cf054956de010fd8f929feb890230
Jesus <chuy8jay@gmail.com>
2015-12-22 04:14:03 -0700

    Able to rate N amount of tweets

--- main.py
+++ main.py
@@ -1,75 +1,54 @@
-from xml_parser import get_categories
-from enum import Enum
+from pyspark.sql import SQLContext
+from pyspark import SparkContext, SparkConf
+from personality_calc import rate_tweet
+from xml_parser import get_categories 
 
-userRatng = dict()
-dtweet = dict()
+filename = 'output.txt'
 
-class pers(Enum):
-    na = 1
-    ma = 2
-    ps = 3
-    op = 4
-    co = 5
-    ex = 6
-    ag = 7
-    ne = 8
-    lst = 9
+conf = SparkConf()
+conf.setMaster("local[4]")
+conf.setAppName("reduce")
+conf.set("spark.executor.memory", "4g")
 
-# The list of all of the available categories
+sc = SparkContext(conf=conf)
+
+sqlContext = SQLContext(sc)
+f = sqlContext.jsonFile(filename)
+
+
+# Get the location and map it to the word count of the tweet
+def map_to_count(s):
+	word_list = s['text'].split(' ')
+	result_d = dict([])
+	for word in word_list:
+		word = word.lower()
+		if(word in result_d):
+			result_d[word] += 1
+		else:
+			result_d[word] = 1
+	return (s['place'].asDict()['full_name'], result_d)
+
+
+# Convert to CSV
+def toCSVLine(data):
+	try:
+		result = str(data[0]) + ',' + ','.join(str(c) for c in data[1].values()) + ',\n'
+	except:
+		return 'BLAMO'
+	return result
+
+
+a = f.map(lambda x: x.asDict())				# Turn JSON output into dictionary
+b = a.filter(lambda x: x['text'] != None and x['place'] != None and x['place'].asDict()['full_name'] != None)		# Filter out non-tweets
+
+
+# Store {location : tweet text} for all tweets
+loc_n_text = b.map(map_to_count)
+
 list_of_cats = get_categories()
 
+# Store {location : score} for all tweets
+loc_n_score = loc_n_text.map(lambda x: (x[0], rate_tweet(x[1], list_of_cats)))
 
-def update_ratings(category, num):
-    for e in pers:
-        if(e is not pers.lst):
-            try:                   
-                increase = num * float(category[e.value][1])
-                userRatng[e.name] = increase;
-            except ValueError:
-                print "Could not parse" + \
-                    category[e.value][1] + "as number"
-
-def split_words(s):
-    word_list = s.split(' ')
-    return word_list
-
-def check_personality(str):
-    map(lambda category: check_personality1(str, category, dtweet), list_of_cats)
-
-def check_personality1(str, category, dict_tweet):
-    word = str.lower() 
-    words = map(lambda x: x.lower(), category[pers.lst.value])
-
-    if word in words and word in dict_tweet:
-        update_ratings(category, dict_tweet[word])
-        return True;
-    else:
-        return False;
-
-def rate_tweet(dict_tweet, usr_Ratings):
-
-    global dtweet
-    dtweet = dict_tweet
-    global userRatng
-    userRatng = usr_Ratings
-
-    # Map through all of the words in a single tweet
-    map(check_personality, dtweet)
-    return userRatng
-
-
-
-# User's personality rating
-uuu_userRatng = dict([
-        ('na', 0), ('ma', 0), ('ps', 0), ('op', 0), \
-            ('co', 0), ('ex', 0), ('ag', 0), ('ne', 0)]) # Shoud come from Greg's code
-
-#tweet = "I am a fake tweet. She went swimming on the lake, we went swimming on the lake"
-tweet = "today was a fun day"
-
-tttweet = tweet.lower().split(' ')
-
-# Tweet should come in, in the form of (word, word_count)
-ddd_dtweet = dict.fromkeys(tttweet, 1) # Should come from Gregs code
-
-print rate_tweet(ddd_dtweet, uuu_userRatng)
+lines = loc_n_score.map(toCSVLine)
+lines.saveAsTextFile('BAM.csv')
