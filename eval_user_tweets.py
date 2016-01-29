f09abc3b8a616923109832382fb059762ccf3384
Jesus <chuy8jay@gmail.com>
2016-01-29 00:49:36 -0700

    Terminates gracefully when it can't connect to the twitter api

--- eval_user_tweets.py
+++ eval_user_tweets.py
@@ -4,7 +4,15 @@ import urllib2
 
 screen_name = sys.argv[1]
 
+class MyException(Exception):
+    pass
+
+
 url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?' + screen_name + '=twitterapi'
 
+try:
+    user_feed = urllib2.urlopen(url, timeout = 1).read()
+except urllib2.URLError as e:
+    print 'Unable to connect to ' + screen_name + '\n'
 
-user_feed = urllib2.urlopen(url).read()
+
