ba309ef37af23d06defedd0bc251f940df1874d4
Jesus <chuy8jay@gmail.com>
2016-02-19 20:29:03 -0700

    Writing formatted json to output file

--- eval_user_tweets.py
+++ eval_user_tweets.py
@@ -26,17 +26,23 @@ api = tweepy.API(auth)
 def getTweets(user_handler):
     stuff = api.user_timeline(screen_name = user_handler, count = 100, include_rts = True)
 
+    tweets = ''
     for s in stuff:
         status = s
-
+        
         jsonStr = json.dumps(status._json)
         j = json.loads(jsonStr)
 
-        print (j['text'])
+        t = j['text']
+        print (t)
+        tweets += t + '\n'
 
-    
-    with open('output.json', 'w') as output_file:
-        output_file.write(json.dumps(status._json))
+
+    print (tweets)
+
+    clean_tweets = tweets.encode('ascii', 'ignore').decode('ascii')
+    #with open('output.txt', 'w') as f:
+     #   f.write(clean_tweets)
 
 
 getTweets('villordoos')
