4b55d3fee656944069beee629a31d98eec2f21e7
Jesus <chuy8jay@gmail.com>
2016-02-19 20:05:25 -0700

    Successfully extracting the text out of each individual tweet

--- eval_user_tweets.py
+++ eval_user_tweets.py
@@ -2,13 +2,10 @@
 
 from __future__ import absolute_import, print_function
 import sys
-import urllib2
 
 #tweepy
-#from tweepy import Stream
-#from tweepy import OAuthHandler
-#from tweepy.streaming import StreamListener
 import tweepy
+import json
 from credentials import getConsumerKey
 from credentials import getConsumerSecret
 from credentials import getAccessToken
@@ -19,21 +16,6 @@ consumer_secret = getConsumerSecret()
 access_token = getAccessToken()
 access_secret = getAccessSecret()
 
-
-'''
-class listener(StreamListener):
-    def on_data(self, data):
-        print data
-        return True
-
-    def on_error(self, status):
-        print status
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["car"])
-'''
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.secure = True
 auth.set_access_token(access_token, access_secret)
@@ -42,7 +24,19 @@ api = tweepy.API(auth)
 
 
 stuff = api.user_timeline(screen_name = 'villordoos', count = 100, include_rts = True)
+#stuff = api.user_timeline(screen_name = 'villordoos', count = 1, include_rts = True)
 
-print(stuff)
 
+
+for s in stuff:
+    status = s
+
+    jsonStr = json.dumps(status._json)
+    j = json.loads(jsonStr)
+
+    print (j['text'])
+
+
+with open('output.json', 'w') as output_file:
+    output_file.write(json.dumps(status._json))
 
