df194fa79650e822fdc36eb504ef2b1d4ba88ff5
Jesus <chuy8jay@gmail.com>
2016-06-07 22:02:46 -0600

    Able to access credentials from keys.txt file

---
+++ credentials.py
@@ -0,0 +1,71 @@
+import os.path
+
+
+keyVars = ['consumerKey', 'consumerSecret', 'accessToken', 'accessSecret']
+availablekeys = []
+
+def errorOutput(lines):
+    for line in lines:
+        s = line.split()
+        if len(s) > 0:
+            k = s[0]
+            availablekeys.append(k)
+
+    missing = list(set(keyVars) - set(availablekeys))
+    print 'You\'re missing the following credential keys: '
+    for m in missing:
+        print m
+
+def checkForKeys():
+    fname = 'keys.txt'
+    global keyVals
+    keyVals = {}
+    if os.path.isfile(fname):
+        with open(fname) as f:
+            lines = f.readlines()
+
+            for line in lines:
+                s = line.split()
+                keyVals[s[0]] = s[2]
+                if len(line.split()) < 1:
+                    errorOutput(lines)
+                    break
+    else:
+        target = open(fname, 'w')
+        
+        consumerKey = raw_input('Enter \'consumerKey\': ')
+        target.write('consumerKey = ' + consumerKey + '\n')
+        keyVals['consumerKey'] = consumerKey
+
+        consumerSecret = raw_input('Enter \'consumerSecret\': ')
+        target.write('consumerSecret = ' + consumerSecret + '\n')
+        keyVals['consumerSecret'] = consumerSecret
+
+        accessToken = raw_input('Enter \'accessToken\': ')
+        target.write('accessToken = ' + accessToken + '\n')
+        keyVals['accessToken'] = accessToken
+
+        accessSecret = raw_input('Enter \'accessSecret\': ')
+        target.write('accessSecret = ' + accessSecret + '\n')
+        keyVals['accessSecret'] = accessSecret
+
+        target.close()
+
+
+
+def getConsumerKey():
+    checkForKeys()
+    print keyVals
+    return keyVals['consumerKey']
+
+def getConsumerSecret():
+    checkForKeys()
+    return keyVals['consumerSecret']
+
+def getAccessToken():
+    checkForKeys()
+    return keyVals['accessToken']
+
+def getAccessSecret():
+    checkForKeys()
+    return keyVals['accessSecret']
